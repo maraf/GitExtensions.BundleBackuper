52e837bffa396523bfad7adfc7f9c58ed5db3af0
Marek Fišera <mara@neptuo.com>
2018-04-26 18:41:15 +0200

    Plugin base with configuration.

---
+++ src/GitExtensions.BundleBackuper/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using GitUIPluginInterfaces;
+using ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExtensions.BundleBackuper
+{
+    public class Plugin : GitPluginBase, IGitPluginForRepository
+    {
+        internal PluginSettings Configuration { get; private set; }
+
+        public Plugin()
+        {
+            Name = "Bundle Backuper";
+            Description = "Branch backuping extension for GitExtensions";
+        }
+
+        public override bool Execute(GitUIBaseEventArgs gitUiCommands)
+            => true;
+
+        public override IEnumerable<ISetting> GetSettings()
+            => Configuration;
+
+        public override void Register(IGitUICommands gitUiCommands)
+        {
+            base.Register(gitUiCommands);
+            Configuration = new PluginSettings(Settings);
+        }
+
+        public override void Unregister(IGitUICommands gitUiCommands)
+        {
+            base.Unregister(gitUiCommands);
+        }
+    }
+}
---
+++ src/GitExtensions.BundleBackuper/PluginSettings.cs
@@ -0,0 +1,44 @@
+﻿using GitUIPluginInterfaces;
+using Neptuo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExtensions.BundleBackuper
+{
+    internal class PluginSettings : IEnumerable<ISetting>
+    {
+        public static StringSetting BackupPathProperty { get; } = new StringSetting("Backup Path", "A path where to store and load from bundle backups.", null);
+
+        private readonly ISettingsSource source;
+
+        public PluginSettings(ISettingsSource source)
+        {
+            Ensure.NotNull(source, "source");
+            this.source = source;
+        }
+
+        #region IEnumerable<ISetting>
+
+        private static readonly List<ISetting> properties;
+
+        static PluginSettings()
+        {
+            properties = new List<ISetting>(1)
+            {
+                BackupPathProperty
+            };
+        }
+
+        public IEnumerator<ISetting> GetEnumerator()
+            => properties.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        #endregion
+    }
+}
