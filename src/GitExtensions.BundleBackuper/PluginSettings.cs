6152874a01611eab8fe25f9c3f9cbc0513901308
Marek Fišera <mara@neptuo.com>
2018-04-30 00:30:40 +0200

    #9 - Add configuration property.

--- src/GitExtensions.BundleBackuper/PluginSettings.cs
+++ src/GitExtensions.BundleBackuper/PluginSettings.cs
@@ -12,12 +12,14 @@ namespace GitExtensions.BundleBackuper
     internal class PluginSettings : IEnumerable<ISetting>
     {
         public static StringSetting BackupPathProperty { get; } = new StringSetting("Backup Path", "Bundle Backup Path", null);
-        public static StringSetting AfterAddRemoteProperty { get; } = new StringSetting("After Add Remote", "Command to run after remote is added ({0} = remote name)", " fetch --progress \"{0}\"");
+        public static StringSetting BackupTemplateProperty { get; } = new StringSetting("Backup Template", "Bundle Name Template", "{Branch.Name}.bundle");
+        public static StringSetting AfterAddRemoteProperty { get; } = new StringSetting("After Add Remote", "Command to run after remote is added ({0} = remote name)", "fetch --progress \"{0}\"");
         public static StringSetting AfterRemoveRemoteProperty { get; } = new StringSetting("After Add Remote", "Command to run after remote is removed ({0} = remote name)", "fetch --all");
 
         private readonly ISettingsSource source;
 
         public string BackupPath => source.GetValue(BackupPathProperty.Name, BackupPathProperty.DefaultValue, t => t);
+        public string BackupTemplate => source.GetValue(BackupTemplateProperty.Name, BackupTemplateProperty.DefaultValue, t => t);
         public string AfterAddRemote => source.GetValue(AfterAddRemoteProperty.Name, AfterAddRemoteProperty.DefaultValue, t => t);
         public string AfterRemoveRemote => source.GetValue(AfterRemoveRemoteProperty.Name, AfterRemoveRemoteProperty.DefaultValue, t => t);
 
@@ -36,6 +38,7 @@ namespace GitExtensions.BundleBackuper
             properties = new List<ISetting>(1)
             {
                 BackupPathProperty,
+                BackupTemplateProperty,
                 AfterAddRemoteProperty,
                 AfterRemoveRemoteProperty
             };
