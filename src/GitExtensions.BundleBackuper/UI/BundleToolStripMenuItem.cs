790c499def4d256a709ff2c486f51d74114bb53b
Marek Fišera <mara@neptuo.com>
2018-05-03 22:11:21 +0200

    #4 - Revert search box implementation.

--- src/GitExtensions.BundleBackuper/UI/BundleToolStripMenuItem.cs
+++ src/GitExtensions.BundleBackuper/UI/BundleToolStripMenuItem.cs
@@ -17,9 +17,6 @@ namespace GitExtensions.BundleBackuper.UI
         private readonly IBundleProvider provider;
         private readonly IGitBundleMapper mapper;
 
-        private readonly ToolStripTextBox searchBox;
-        private IEnumerable<Bundle> currentBundles;
-
         internal BundleToolStripMenuItem(IBundleProvider provider, IGitBundleMapper mapper, IGitBundleFactory bundleFactory)
         {
             Ensure.NotNull(provider, "provider");
@@ -29,29 +26,9 @@ namespace GitExtensions.BundleBackuper.UI
 
             Text = "Bundles";
             DropDownOpening += OnDropDownOpening;
-            DropDownOpened += OnDropDownOpened;
             DropDownItemClicked += OnDropDownItemClicked;
 
-            searchBox = new ToolStripTextBox()
-            {
-                AutoSize = false,
-                Width = 200,
-                ToolTipText = "Search bundles...",
-            };
-            searchBox.TextChanged += OnSearchBoxTextChanged;
-            DropDown.Items.Add(searchBox);
-            DropDown.Items.Add(new ToolStripSeparator());
             DropDown.Items.Add(new AddButton(bundleFactory));
-        }
-
-        private void OnSearchBoxTextChanged(object sender, EventArgs e)
-        {
-            string searchText = searchBox.Text.ToLowerInvariant();
-            for (int i = 1; i < DropDown.Items.Count; i++)
-            {
-                if (DropDown.Items[i] is ToolStripMenuItem item && item.Tag is Bundle bundle)
-                    item.Visible = bundle.Name.ToLowerInvariant().Contains(searchText);
-            }
         }
 
         private async void OnDropDownOpening(object sender, EventArgs e)
@@ -59,21 +36,21 @@ namespace GitExtensions.BundleBackuper.UI
             foreach (var item in DropDown.Items.OfType<ToolStripItem>().Where(i => i.Tag is Bundle).ToList())
                 DropDown.Items.Remove(item);
 
-            currentBundles = await provider.EnumerateAsync();
+            if (DropDown.Items.Count == 2)
+                DropDown.Items.RemoveAt(1);
+
+            IEnumerable<Bundle> currentBundles = await provider.EnumerateAsync();
             foreach (Bundle bundle in currentBundles)
             {
-                DropDown.Items.Insert(1, new ToolStripMenuItem(bundle.Name)
+                if (DropDown.Items.Count <= 1)
+                    DropDown.Items.Add(new ToolStripSeparator());
+
+                DropDown.Items.Add(new ToolStripMenuItem(bundle.Name)
                 {
                     Tag = bundle,
                     Checked = mapper.Has(bundle)
                 });
             }
-        }
-
-        private void OnDropDownOpened(object sender, EventArgs e)
-        {
-            searchBox.Text = String.Empty;
-            searchBox.Focus();
         }
 
         private void OnDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
